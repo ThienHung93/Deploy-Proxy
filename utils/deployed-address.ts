9dd9f27307e826b95554a44a8ba0e19099cde238
ThienHung93 <142766170+ThienHung93@users.noreply.github.com>
2024-08-09 14:43:53 +0000

    update

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x464F0aF21A720ECeF078333949A943784c46c161'
+export const ProxyAdmin = '0x92e6BF717e632F18ebE5ADD1546e7bd3FEC1aF82'
+export const SWTRImplementation = '0xdeF1E260b7167410e9aE2C69b350E813904801F8'
+    
